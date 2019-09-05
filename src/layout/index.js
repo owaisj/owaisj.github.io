b7cd5252d9daf7331b1b99cd56363e4809b8898d
owaisj <jamil.owais@gmail.com>
2019-09-05 18:30:21 -0500

    Document title and favicon

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-export default ({ children, title }) => (
-  <div id="root-container" className="d-flex flex-column">
-    <Header />
-    <div id="content-wrapper" className="flex-fill container-fluid bg-light">
-      <div
-        style={{
-          minHeight: '500px',
-        }}
-        className="row d-flex justify-content-center m-3"
-      >
-        <div id="content" class="col col-md-10 col-sm">
-          <h1 className="border-bottom border-dark display-4">{title}</h1>
-          {children}
+export default ({ children, title }) => {
+  useEffect(() => {
+    document.title = 'Owais Jamil'
+  }, [])
+  return (
+    <div id="root-container" className="d-flex flex-column">
+      <Header />
+      <div id="content-wrapper" className="flex-fill container-fluid bg-light">
+        <div
+          style={{
+            minHeight: '500px',
+          }}
+          className="row d-flex justify-content-center m-3"
+        >
+          <div id="content" class="col col-md-10 col-sm">
+            <h1 className="border-bottom border-dark display-4">{title}</h1>
+            {children}
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-)
+  )
+}
